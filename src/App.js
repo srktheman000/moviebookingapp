fefdd9cf1199c793f04a936cd6c984e191af4154
sohan <sohan.kinage000@gmail.com>
2022-08-12 01:41:19 +0530

    added th folder sructure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Authentication from "./pages/authentication/Authentication";
 
 function App() {
-  return <div className="App">WELOCOME TO MOVIE BOOKING APP</div>;
+  return (
+    <div className="App">
+      <Authentication />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/authentication/Authentication.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./aithentication.css";
+
+const Authentication = () => {
+  return <div>Authentication</div>;
+};
+
+export default Authentication;
